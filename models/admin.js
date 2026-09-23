95e46553b64e7773069bda6ae187e0d3dfbfb743
humblesoul150 <kisibojonathan150@gmail.com>
2026-09-23 12:10:47 +0300

    added new roles in admin model

--- models/admin.js
+++ models/admin.js
@@ -6,8 +6,7 @@ const adminSchema = new mongoose.Schema(
     password: { type: String, required: true },
     role: {
       type: String,
-      enum: ["admin", "blogger", "viewer"], 
-      
+      enum: ["developer", "admin", "editor"],
       default: "admin",
     },
     loggedIn: { type: Boolean, default: false },
